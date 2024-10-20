92d5a1dac7716e963a068b4f5fc2e3b7e847bf85
ayat <ayat@ayats-MacBook-Air.fritz.box>
2024-10-20 20:04:58 +0200

    lab complete

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5;
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
@@ -22,10 +22,14 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const lastLetterPart1= part1.charAt(part1.length-1).toUpperCase()
+                        //part1.slice(0, part1.length - 1) + part1.slice(-1).toUpperCase()
+const lastLetterPart2= part2.charAt(part1.length-1).toUpperCase()
+                        //part2.slice(0, part2.length - 1) + part2.slice(-1).toUpperCase();
 
 // Print the cameLtaiL-formatted string
-
+const cameLtaiL = part1.slice(0,-1) + lastLetterPart1 + part2.slice(0,-1) + lastLetterPart2;
+console.log(cameLtaiL);
 
 
 
@@ -35,10 +39,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,10 +51,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 11); 
+//const randomNumber = Math.floor(Math.random() * 10) + 1; beause random*10 will only do until 9 
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -61,16 +66,25 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; false
 
-const expression2 = a || b;
+const expression2 = a || b; true 
 
-const expression3 = !a && b;
+const expression3 = !a && b; false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); false
 
-const expression7 = a && a;
+const expression7 = a && a; true
+
+
+
+
+
+
+
+
+
